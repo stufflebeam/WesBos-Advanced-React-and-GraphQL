24565511110804b2dc9aa23ec21140de0fa6ff6d
Will Stufflebeam <git@stufflebeam.co>
2022-08-24 01:27:49 -0600

    [vid 8] Wired up Router & NProgress in _app.js

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,5 +1,14 @@
 import PropTypes from 'prop-types';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Page from '../components/Page';
+
+// import 'nprogress/nprogress.css'; // Official NProgress progress bar style
+import '../components/styles/nprogress.css'; // Our own custom NProgress progress bar style
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
