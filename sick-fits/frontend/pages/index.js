efb9bff8183f292b5b79fc5a12f91935bb4f1f76
Will Stufflebeam <git@stufflebeam.co>
2022-08-24 00:41:03 -0600

    [vid 6] Added styled components in Header & index

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,8 +1,23 @@
+import Form from '../components/styles/Form';
+
 export default function IndexPage() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log('[Index] Form submitted: ', e.target);
+  };
   return (
     <>
       <h1>Hello World</h1>
       <p>This is a simple example of a React component.</p>
+      <Form onSubmit={handleClick}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <button type="submit">Submit</button>
+      </Form>
     </>
   );
 }
