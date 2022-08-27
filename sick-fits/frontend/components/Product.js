cb54b8abcfe5bc1db3b0ff6914c5b03b1c791fe1
Will Stufflebeam <git@stufflebeam.co>
2022-08-27 07:18:28 +0000

    [vid 20] Further styled display of products in grid

---
+++ sick-fits/frontend/components/Product.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import formatMoney from '../lib/formatMoney';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import Title from './styles/Title';
+
+// This is one example of a placeholder image that can be used when a product
+// doesn't have a photo.
+// TODO: Ideally, we would use different placeholder images for different products.
+// For example, we could use a shoe placeholder image for shoes, a hat placeholder
+// image for hats, and so on.
+// This would probably be easiest if we added a 'category' field to each of the products.
+// If a product can be in multiple categories, we could designate one as the 'primary'
+// or just use the first one. Not that important at the moment, but it's something to
+// keep in mind, as having category-tailored placeholder images would add to the
+// fit and finish of the site.
+// For now though, we'll just use the same placeholder image for all products.
+const placeholder =
+  'https://res.cloudinary.com/stufflebeam/image/upload/v1661582104/placeholder/placeholder-images-product-2_large.webp';
+
+// TODO: Once the product grid is fully styled, if there is still an issue with long
+//       descriptions looking terrible, then we should truncate them at word boundaries
+//       and add an ellipsis.
+//       We can wire that up ourselves, but it'd be quickest and easiest to just use
+//       something like this:
+//       https://www.npmjs.com/package/ellipsize
+//       If we end up supporting HTML in descriptions, then we'll need to use something
+//       like this instead:
+//       https://www.npmjs.com/package/react-truncate-html
+//       It might also be better to just return the first line of the description
+//       or add a 'short description' field or a 'subhead' field to the products table.
+
+// const Product = ({ product }) => (
+export default function Product({ product }) {
+  return (
+    <ItemStyles key={product.id}>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed || placeholder}
+        alt={product?.photo?.altText || product?.name}
+      />
+      <Title>
+        <Link href={`/products/${product.id}`}>{product.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      {/* TODO: Add buttons to edit and delete items */}
+    </ItemStyles>
+  );
+}
+
+// export default Product;
--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -6,7 +6,9 @@ import gql from 'graphql-tag';
 // unsure what's going on here. :(
 import { useQuery } from '@apollo/client';
 import ReactLoading from 'react-loading';
+import styled from 'styled-components';
 import LoadingAnimation from './LoadingAnimation';
+import Product from './Product';
 
 const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
@@ -26,17 +28,29 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  /* max-width: ${(props) => props.theme.maxWidth};
+  margin: 0 auto; */
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
-  console.log('[Products]: data:', data);
-  console.log('[Products]: error:', error);
-  console.log('[Products]: loading:', loading);
+  //   console.log('[Products]: data:', data);
+  //   console.log('[Products]: error:', error);
+  //   console.log('[Products]: loading:', loading);
 
   if (loading) return <LoadingAnimation config={{ color: '#FF0000' }} />;
   if (error) return <p>Error: {error.message}</p>;
   return (
     <div>
-      <h1>Products component</h1>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsListStyles>
     </div>
   );
 }
