710fa96733ef6e8d6ec0c34e66b28042299ddf43
Will Stufflebeam <git@stufflebeam.co>
2022-08-27 04:47:43 +0000

    [vid 20] Updated index to display product page Updated the index page to display the same content as the product page

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,23 +1,3 @@
-import Form from '../components/styles/Form';
+import ProductsPage from './products';
 
-export default function IndexPage() {
-  const handleClick = (e) => {
-    e.preventDefault();
-    console.log('[Index] Form submitted: ', e.target);
-  };
-  return (
-    <>
-      <h1>Hello World</h1>
-      <p>This is a simple example of a React component.</p>
-      <Form onSubmit={handleClick}>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-        <button type="submit">Submit</button>
-      </Form>
-    </>
-  );
-}
+export default ProductsPage;
