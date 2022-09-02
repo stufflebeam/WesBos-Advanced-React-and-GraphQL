95a9524165dae5249144ae5383de1d9a6a671aa7
Will Stufflebeam <git@stufflebeam.co>
2022-09-02 10:36:31 +0000

    [vid 59] Refined the display of the SingleOrder component Reworked the SingleOrder component so that its output matches that of Wes' code in the tutorial. I opted to do this, rather than spending an inordinate amount of time customizing this on my own, because he had already thought through all of the data and written the custom styles to present it nicely.

--- sick-fits/frontend/components/SingleOrder.js
+++ sick-fits/frontend/components/SingleOrder.js
@@ -5,32 +5,33 @@ import styled from 'styled-components';
 import DisplayError from './ErrorMessage';
 import LoadingAnimation from './LoadingAnimation';
 import formatMoney from '../lib/formatMoney';
+import OrderStyles from './styles/OrderStyles';
 
-const OrderStyles = styled.div`
-  /* display: grid;
-  grid-auto-columns: 1fr;
-  grid-auto-flow: column;
-  max-width: (--maxWidth);
-  justify-content: center;
-  align-items: start;
-  gap: 2rem;
-  img {
-    width: 100%;
-    object-fit: contain;
-  } */
-  .order-item {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-gap: 1rem;
-    margin-bottom: 1rem;
-    border-bottom: 1px solid var(--lightGrey);
-    padding-bottom: 1rem;
-    img {
-      width: 300px;
-      /* object-fit: contain; */
-    }
-  }
-`;
+// const OrderStyles = styled.div`
+//   /* display: grid;
+//   grid-auto-columns: 1fr;
+//   grid-auto-flow: column;
+//   max-width: (--maxWidth);
+//   justify-content: center;
+//   align-items: start;
+//   gap: 2rem;
+//   img {
+//     width: 100%;
+//     object-fit: contain;
+//   } */
+//   .order-item {
+//     display: grid;
+//     grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+//     grid-gap: 1rem;
+//     margin-bottom: 1rem;
+//     border-bottom: 1px solid var(--lightGrey);
+//     padding-bottom: 1rem;
+//     img {
+//       width: 300px;
+//       /* object-fit: contain; */
+//     }
+//   }
+// `;
 
 export const SINGLE_ORDER_QUERY = gql`
   query SINGLE_ORDER_QUERY(
@@ -73,14 +74,51 @@ export default function SingleOrder({ id }) {
   //   if (error) return <p>Error: {error.message}</p>;
   if (error) return <DisplayError error={error} />;
   const { id: orderId, user, total, charge, items } = data.Order;
+  const totalNumberOfItems = items.reduce(
+    (tally, item) => tally + item.quantity,
+    0
+  );
   return (
     <OrderStyles>
       <Head>
         {/* TODO: Come up with a better, user-facing Order Id to use */}
         <title>Sick Fits | Order {orderId}</title>
       </Head>
+      <p>
+        <span>Order Id:</span>
+        <span>{orderId}</span>
+      </p>
+      <p>
+        <span>Charge:</span>
+        <span>{charge}</span>
+      </p>
+      <p>
+        <span>Order Total:</span>
+        <span>{formatMoney(total)}</span>
+      </p>
+      <p>
+        <span>Item Count:</span>
+        <span>{totalNumberOfItems}</span>
+      </p>
+      <div className="items">
+        {items.map((item) => (
+          <div key={item.id} className="order-item">
+            <img
+              src={item.photo.image.publicUrlTransformed}
+              alt={item.photo.altText}
+            />
+            <div className="item-details">
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <p>Qty: {item.quantity}</p>
+              <p>Each: {formatMoney(item.price)}</p>
+              <p>Subtotal: {formatMoney(item.price * item.quantity)}</p>
+            </div>
+          </div>
+        ))}
+      </div>
       {/* <DisplayError error={error} /> */}
-      <h1>Order #{orderId}</h1>
+      {/* <h1>Order #{orderId}</h1>
       <hr />
       <p>Order Details</p>
       <p>Order Total: {formatMoney(total)}</p>
@@ -101,7 +139,7 @@ export default function SingleOrder({ id }) {
             <p>Description: {item.description}</p>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </OrderStyles>
   );
 }
