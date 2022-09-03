c71b92d00b43093a476e0245a31ccda12b50ea5c
Will Stufflebeam <git@stufflebeam.co>
2022-09-03 06:03:19 +0000

    [vid 66] Added Roles-related permissions and Admin UI settings

--- sick-fits/backend/schemas/Role.ts
+++ sick-fits/backend/schemas/Role.ts
@@ -1,12 +1,22 @@
 import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { permissions } from '../access';
 import { permissionFields } from './fields';
 
 export const Role = list({
+    access: {
+        create: permissions.canManageRoles,
+        read: permissions.canManageRoles,
+        update: permissions.canManageRoles,
+        delete: permissions.canManageRoles,
+    },
     ui: {
         listView: {
             initialColumns: ['name', 'assignedTo'],
         },
+        hideCreate: (args) => !permissions.canManageRoles(args),
+        hideDelete: (args) => !permissions.canManageRoles(args),
+        isHidden: (args) => !permissions.canManageRoles(args),
     },
     fields: {
         name: text({ isRequired: true }),
